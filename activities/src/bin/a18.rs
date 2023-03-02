a60b128fa380e0f4922d7f80ddaa6097b5ae1789
Chantel Punt <csky.punt@gmail.com>
2023-03-02 16:16:45 +1000

    result exercise and challenge

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -16,4 +16,36 @@
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
 
-fn main() {}
+#[derive(Debug)]
+struct Adult {
+    age: u8,
+    name: String,
+}
+
+impl Adult {
+    fn new(age: u8, name: &str) -> Result<Self, &str> {
+        if age >= 21 {
+            Ok(Self {
+                age,
+                name: name.to_string()
+            })
+        } else {
+            Err("Age must be at least 21")
+        }
+    }
+}
+
+fn main() {
+    let child = Adult::new(15, "Apollo");
+    let adult = Adult::new(21, "Shadow");
+
+    match child {
+        Ok(child) => println!("{} is {} years old", child.name, child.age),
+        Err(e) => println!("{e}"),
+    }
+
+    match adult {
+        Ok(a) => println!("{} is {} years old", a.name, a.age),
+        Err(e) => println!("{e}"),
+    }
+}
