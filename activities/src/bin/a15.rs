3c74869e494a093cbaf9fe4cae090f65e1aad653
Chantel Punt <csky.punt@gmail.com>
2023-03-02 14:43:18 +1000

    activity 15

--- activities/src/bin/a15.rs
+++ activities/src/bin/a15.rs
@@ -8,7 +8,25 @@
 //
 // Notes:
 // * Use an enum for the tickets with data associated with each variant
-// * Create one of each ticket and place into a vector
-// * Use a match expression while iterating the vector to print the ticket info
+enum Ticket {
+    Backstage(f64, String),
+    Standard(f64),
+    Vip(f64, String),
+}
 
-fn main() {}
+fn main() {
+    // * Create one of each ticket and place into a vector
+    let tickets = vec![
+        Ticket::Backstage(307.20, "Sky".to_owned()),
+        Ticket::Standard(100.00),
+        Ticket::Vip(250.00, "Apollo".to_owned())
+    ];
+    // * Use a match expression while iterating the vector to print the ticket info
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => println!("Backstage ticket holder: {:?}, price: {:?}", holder, price),
+            Ticket::Standard(price) => println!("standard ticket price: {:?}", price),
+            Ticket::Vip(price, holder) => println!("VIP ticket holder: {:?}, price: {:?}", holder, price),
+        }
+    }
+}
